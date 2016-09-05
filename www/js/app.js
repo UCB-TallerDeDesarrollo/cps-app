e44ce26c8c42976306bb90580dd9703fbcc5ce36
vildozo <vildozo@outlook.com>
2016-09-05 11:59:20 -0400

    commiting before laptop shutdowns,

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -49,25 +49,25 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
-    .state('app.playlists', {
-      url: '/playlists',
+    .state('app.laggingSkills', {
+      url: '/laggingSkills',
       views: {
         'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
+          templateUrl: 'templates/laggingSkills.html',
+          controller: 'LaggingSkillsCtrl'
         }
       }
     })
 
   .state('app.single', {
-    url: '/playlists/:playlistId',
+    url: '/laggingSkills/:laggingSkillId',
     views: {
       'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+        templateUrl: 'templates/laggingSkill.html',
+        controller: 'LaggingSkillCtrl'
       }
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/laggingSkills');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,16 +41,10 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('LaggingSkillsCtrl', function($scope, LaggingSkills) {
+  $scope.laggingSkills = LaggingSkills.all();
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('LaggingSkillCtrl', function($scope, $stateParams) {
+  $scope.params = $stateParams;
 });
