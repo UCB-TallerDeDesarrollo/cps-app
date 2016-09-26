435c0c5abee1e9263815625429b81a566d972427
sergioMoralesPercella <sergio.morales@percella.com>
2016-09-26 03:52:05 -0400

    Modal created

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -128,7 +128,7 @@ angular.module('starter.controllers', [])
 })
 
 
-.controller('EditUnsolvedProblemCtrl', function($scope, $cordovaSQLite, $state){
+.controller('EditUnsolvedProblemCtrl', function($scope, $cordovaSQLite, $state, $ionicModal){
   $scope.unsolvedProblem = {
       description: "",
       id:$state.params.itemId
@@ -150,8 +150,32 @@ angular.module('starter.controllers', [])
     else {
       $scope.emptyInput = true;
     }
-
   };
+
+  $ionicModal.fromTemplateUrl('my-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
 });
 
 // OTHER FUNCTIONS
