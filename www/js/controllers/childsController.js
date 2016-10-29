058183aad984fc85c2d240df03693ebb42170bb6
sergioKmoralesV <sergio.morales@percella.com>
2016-10-29 09:31:01 -0400

    Childs modal added

--- www/js/controllers/childsController.js
+++ www/js/controllers/childsController.js
@@ -1,5 +1,29 @@
 angular.module('starter.controllers')
   .controller('ChildsCtrl', function($scope, $cordovaSQLite, $state, $ionicActionSheet, $ionicListDelegate, $ionicPopup, $ionicModal, $stateParams) {
 
+    $ionicModal.fromTemplateUrl('templates/childs/create-child-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modalCreate = modal;
+    });
+    $scope.openModal = function() {
+      $scope.modalCreate.show();
+    };
+    $scope.closeModal = function() {
+      $scope.modalCreate.hide();
+    };
+    // Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modalCreate.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+      // Execute action
+    });
 
 });
