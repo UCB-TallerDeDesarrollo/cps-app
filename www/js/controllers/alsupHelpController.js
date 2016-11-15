7ff74a855a5a5da3b0f9037ef3f6fec12c7a0daf
Lesly Méndez C <leslicita_mc@hotmail.com>
2016-11-14 21:58:50 -0400

    Improved alsup help

--- www/js/controllers/alsupHelpController.js
+++ www/js/controllers/alsupHelpController.js
@@ -1,6 +1,17 @@
 angular.module('starter.controllers')
 .controller('AlsupCtrl', function($scope, AppTools) {
 
+  $scope.toggleContent = function(content) {
+    if ($scope.isContentShown(content)) {
+      $scope.shownContent = null;
+    } else {
+      $scope.shownContent = content;
+    }
+  };
+  $scope.isContentShown = function(content) {
+    return $scope.shownContent === content;
+  };
+
   $scope.browserInstance = {};
   $scope.browserInstance = AppTools.newBrowser({
     scope: $scope,
@@ -10,27 +21,46 @@ angular.module('starter.controllers')
   $scope.alsupHelpDir = [
     {
       title: "Audio: How to use the ALSUP",
-      url:"http://www.blogtalkradio.com/drrosswgreene/2012/01/23/collaborative-problem-solving-at-school"
+      topics: [
+        { content: "Using the Assessment of Lagging Skills and Unsolved Problems",
+          url:"http://www.blogtalkradio.com/drrosswgreene/2012/01/23/collaborative-problem-solving-at-school"
+        }
+      ]
     },
     {
       title: "Copy of the ALSUP",
-      url:"http://livesinthebalance.org/sites/default/files/ALSUP%20Rev%2011-12-12%20pdf%20%282%29_1.pdf"
+      topics: [
+        { content: "Assessment Of Lagging Skills & Unsolved Problems",
+          url:"http://livesinthebalance.org/sites/default/files/ALSUP%20Rev%2011-12-12%20pdf%20%282%29_1.pdf"
+        }
+      ]
     },
     {
       title: "ALSUP Guide",
-      url:"http://livesinthebalance.org/sites/default/files/ALSUP%20Guide%209-16-14_1.pdf"
+      topics: [
+        { content: "Collaborative & Proactive Solutions - ALSUP Guide",
+          url:"http://livesinthebalance.org/sites/default/files/ALSUP%20Guide%209-16-14_1.pdf"
+        }
+      ]
     },
     {
-      title: "Help with the wording of unsolved problems [1]",
-      url:"http://www.blogtalkradio.com/dr-ross-greene/2012/10/15/parenting-your-challenging-child"
-    },
-    {
-      title: "Help with the wording of unsolved problems [2]",
-      url:"http://www.blogtalkradio.com/dr-ross-greene/2012/05/15/parenting-your-challenging-child"
+      title: "Help with the wording of unsolved problems",
+      topics: [
+        { content: "Precision on Unsolved Problems",
+          url:"http://www.blogtalkradio.com/dr-ross-greene/2012/10/15/parenting-your-challenging-child"
+        },
+        { content: "Just Getting Started: Identifying Unsolved Problems",
+          url:"http://www.blogtalkradio.com/dr-ross-greene/2012/05/15/parenting-your-challenging-child"
+        }
+      ]
     },
     {
       title: "Video of ALSUP meeting",
-      url:"http://www.livesinthebalance.org/ALSUP-meeting#overlay-context=walking-tour-parents"
+      topics: [
+        { content: "ALSUP Meeting (Video)",
+          url:"http://www.livesinthebalance.org/ALSUP-meeting#overlay-context=walking-tour-parents"
+        }
+      ]
     }
   ];
 });
