66fe5170f74d0c63f29090e5a1ca62953434308c
Daniel Illanes <dany.gaara.ds@gmail.com>
2017-11-21 19:23:35 -0400

    i7.22 Como usuario quiero que se agreguen mas pistas al boton de ayuda - implementado

--- www/languages/en.js
+++ www/languages/en.js
@@ -71,6 +71,7 @@ var $translations_en = {
     UnsolvedProblemHintTitle: "Unsolved problem hint",
     UnsolvedProblemHintBodyPart1: "An unsolved problem is an expectation your child is having difficulty reliably meeting.",
     UnsolvedProblemHintBodyPart2: "Each lagging skill can have multiple unsolved problems, one, or even none. Each unsolved problem can be associated with multiple lagging skills, but only needs to be entered once.",
+    UnsolvedProblemHintBodyPart3: "After you've made a comprehensive list of unsolved problems, click on the arrow next to the one you want to solve first.",
     Got_It: "Got it",
     Tell_me_more: "Tell me more",
     goingTo: "Going to",
